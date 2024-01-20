a94b69bd705157cb77a3bf807ef127369384846b
James Dryden <jdryden572@gmail.com>
2024-01-20 16:16:08 -0500

    Create template repository

---
+++ README.md
@@ -0,0 +1,12 @@
+# Razor Pages with Svelte template
+
+Template for a Razor Pages web app that uses Svelte and Vite for front-end.
+
+Uses:
+- .NET 8
+- ASP.NET Core Razor Pages
+- [Vite.AspNetCore](https://github.com/Eptagone/Vite.AspNetCore)
+- Vite
+- Svelte
+
+To generate a Visual Studio template from this repo, clone it locally and follow the steps to [Use the export template wizard](https://learn.microsoft.com/en-us/visualstudio/ide/how-to-create-project-templates?view=vs-2022).
---
+++ RazorPagesWithSvelte/Program.cs
@@ -0,0 +1,33 @@
+using Vite.AspNetCore.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddViteServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseViteDevelopmentServer(useMiddleware: true);
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
